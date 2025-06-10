ad6a84467c06f84835a48374897ec1376daa756d
jeyavidhya-web <jeyavidhya.mahesh@gmail.com>
2025-06-09 22:58:20 -0500

    base code

--- errors/bad-request.js
+++ errors/bad-request.js
@@ -1,10 +1,10 @@
 const { StatusCodes } = require('http-status-codes');
 const CustomAPIError = require('./custom-api');
 
-class BadRequestError extends Error {
+class BadRequestError extends CustomAPIError {
   constructor(message) {
     super(message);
-    this.statusCode = StatusCodes.BAD_REQUEST
+    this.statusCode = StatusCodes.BAD_REQUEST;
   }
 }
 
--- errors/unauthenticated.js
+++ errors/unauthenticated.js
@@ -1,7 +1,6 @@
 const { StatusCodes } = require('http-status-codes');
 const CustomAPIError = require('./custom-api');
 
-
 class UnauthenticatedError extends CustomAPIError {
   constructor(message) {
     super(message);
